301d24d10715810a172261a294203bedf9714c8f
Tomek Skoczylas <tomasz.m.skoczylas@gmail.com>
2020-07-10 13:00:34 +0200

    inne

---
+++ guplfile.js
@@ -0,0 +1,41 @@
+// Ścieżka do aktualnie wykonywanego zadania
+const entryPath = "Verti_1";
+
+const gulp = require("gulp");11
+const sass = require("gulp-sass");
+sass.compiler = require("sass");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+
+function compileSass(done) {
+    gulp
+        .src(entryPath + "/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(entryPath + "/css"));
+
+    done();
+}
+
+function watcher(done) {
+    browserSync.init({
+        server: "./" + entryPath,
+    });
+
+    gulp.watch(entryPath + "/scss/**/*.scss", gulp.series(compileSass, reload));
+    gulp.watch(entryPath + "/*.html", gulp.series(reload));
+
+    done();
+}
+
+function reload(done) {
+    browserSync.reload();                                                                                                           rowserSync.reload();
+    done();
+}
+
+exports.sass = gulp.parallel(compileSass);
+exports.default = gulp.parallel(compileSass, watcher);
+
